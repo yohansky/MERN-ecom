55c6791a45b84ea008cd84a89aa39ec6a5ba771b
Yohanes <yohanesdahlan@gmail.com>
2024-10-18 18:08:05 +0700

    List Products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="ms-auto">
               <Nav.Link href="/cart">
                 <FaShoppingCart />
                 Cart
