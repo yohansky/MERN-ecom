1ba0052e29dd065059e6c0f9c30b6e064603cf93
Yohanes <yohanesdahlan@gmail.com>
2024-11-17 11:58:37 +0700

    Payment Method

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PrivateRouter from "./components/PrivateRouter";
+import PaymentScreen from "./screens/PaymentScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
 
       <Route path="" element={<PrivateRouter />}>
         <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
       </Route>
     </Route>
   )
