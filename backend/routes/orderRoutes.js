6c3eb681154faf5ac7afcf7cb1cdf1f88951012d
Yohanes <yohanesdahlan@gmail.com>
2024-11-17 16:25:39 +0700

    Order Page

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,7 +5,7 @@ export const router = express.Router();
 
 router.route("/").post(protect, addOrderItems).get(protect, getOrders);
 router.route("/mine").get(protect, getMyOrders);
-router.route("/:id").get(protect, admin, getOrderById);
+router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
 router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
