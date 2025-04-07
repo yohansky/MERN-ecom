80c3afca9eb08aac3d23611acb792ca60f5cf5a1
Yohanes <yohanesdahlan@gmail.com>
2025-04-07 23:00:42 +0700

    list users for admin

---
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { FaTimes, FaTrash, FaEdit, FaCheck } from "react-icons/fa";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import { useGetUsersQuery } from "../../slices/usersApiSlice";
+import { Button, Table } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const UserListScreen = () => {
+  const { data: orders, refetch, isLoading, error } = useGetUsersQuery();
+
+  const deleteHandler = (id) => {
+    console.log("delete");
+  };
+
+  return (
+    <>
+      <h1>Users</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Admin</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>
+                  <a href={`mailto:${user.email}`}>{user.email}</a>{" "}
+                </td>
+                <td>{user.isAdmin ? <FaCheck style={{ color: "green" }} /> : <FaTimes style={{ color: "red" }} />}</td>
+                <td>
+                  <LinkContainer to={`admin/user/${user._id}/edit`}>
+                    <Button variant="light" className="btn-sm">
+                      <FaEdit />
+                    </Button>
+                  </LinkContainer>
+                  <Button variant="danger" className="btn-sm" onClick={() => deleteHandler(user._id)}>
+                    <FaTrash style={{ color: "white" }} />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default UserListScreen;
